9689273a94b57ccf1c6ec38be9a46a4c6f913d3f
Kalle Ryholt <kallecool2608@gmail.com>
2025-03-14 11:35:31 +0100

    opgave A

--- shopping_list.py
+++ shopping_list.py
@@ -19,14 +19,26 @@ class ShoppingList:
         Returns the list of shopping items.
     """
     def __init__(self):
-        self.items = []
+        self.items = {}
 
-    def add_item(self, item):
-        self.items.append(item)
-
-    def remove_item(self, item):
+    def add_item(self, item, quantity=1):
         if item in self.items:
-            self.items.remove(item)
+           self.items[item] += quantity
+        else:
+            self.items[item] = quantity
+
+
+
+    def remove_item(self, item, quantity=1):
+        if item in self.items:
+            if self.items[item] > quantity:
+                self.items[item] -= quantity
+            elif self.items[item] == quantity:
+                del self.items[item]
+            else:
+                print(f"Cannot remove {quantity} {item}(s), only {self.items[item]} available")
+        else:
+            print(f"{item} not found in the shopping list.")
 
     def get_items(self):
         return self.items
@@ -34,7 +46,7 @@ class ShoppingList:
 # Example usage:
 if __name__ == "__main__":
     shopping_list = ShoppingList()
-    shopping_list.add_item("Apples")
+    shopping_list.add_item("Apples",10)
     shopping_list.add_item("Bananas")
     print(shopping_list.get_items())  # Output: ['Apples', 'Bananas']
     shopping_list.remove_item("Apples")
